c35ee20848f17b8f91cf6e0c979fd3bca5825799
LAMorris <leahmorris@Leahs-MacBook-Pro.local>
2018-12-13 14:24:12 -0600

    cleaned up code and reworked some of the logic / worked through some of my pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,20 +8,16 @@ var wordList = ["advent", "christmas", "dreidel", "eggnog", "elves", "gingerbrea
 var currentWord = wordList[Math.floor(Math.random() * wordList.length)]; // Random word is chosen by computer 
     console.log("Current, random word is: " + currentWord); // Checking to see if random word is chosen
 
-// var currentWordElement = document.createElement("span"); // Creating new element for random word chosen and storing it in a variable
-// var currentWordText = document.createTextNode(" " + currentWord); // Creating a new text node for random word chosen and storing it in a variable
-// currentWordElement.appendChild(currentWordText); // Attaching the new text node for random word chosen to the new element created
-// document.getElementById("current-word").appendChild(currentWordElement); // Telling where the new element for random chosen word will go and inserting the new element into its position
-
 //=============================================================================================================================================================================================
 
 // LETTERS ARE DISPLAYED AS UNDERSCORES (__)
-var currentWordGuess = [];
+var blanksArray = [];
 
 for (var i = 0; i < currentWord.length; i++) {
-    currentWordGuess.push("_");
-    document.getElementById("guessing-word").textContent = currentWordGuess.join(" ");
+    blanksArray.push("_");
+    console.log(blanksArray);
   }
+    document.getElementById("guessing-word").innerHTML = blanksArray.join(" ");
 
 //=============================================================================================================================================================================================
 
@@ -55,47 +51,90 @@ document.getElementById("guesses-remaining").appendChild(newGuessesLeft);
 // RUN THIS FUNCTION WHEN USER PRESSES A KEY
 
 // declaring initial variables for use in function
-var userGuess = [];
-var guessCount = "";
+var userGuess = "";
+var guessedLetters = [];
+var guessCount = 10;
 var splitWord = currentWord.split("");
     console.log(splitWord);
-var correctLetters = [];
+var totalLetters = splitWord.length;
+var correctLetters = 0;
 
 // each letter key pressed is stored and listed
-document.onkeypress = function(event) { 
-    userGuess = String.fromCharCode(event.keyCode);
+document.onkeyup = function(event) { 
+    userGuess = String.fromCharCode(event.keyCode).toLowerCase();
         console.log(userGuess);
     
-    newGuessedLetterEl = document.createElement("span");
-    newGuessedText = document.createTextNode(" " + userGuess);
-    newGuessedLetterEl.appendChild(newGuessedText);
-    document.getElementById("current-guesses").appendChild(newGuessedLetterEl);
+    // newGuessedLetterEl = document.createElement("span");
+    // newGuessedText = document.createTextNode(" " + userGuess);
+    // newGuessedLetterEl.appendChild(newGuessedText);
+    // document.getElementById("current-guesses").appendChild(newGuessedLetterEl);
 
-    // each letter key pressed decreases the allowed number of guessed 
-    guessesLeft--;
-        console.log(guessesLeft);
-    document.getElementById("guesses-remaining").innerHTML = ("Guesses Remaining: " + guessesLeft);
+    // verifies if user has picked the same letter twice
+    if (guessedLetters.indexOf(userGuess) >= 0) {
+        console.log("you've already guessed that letter");
+        return; //stop the rest of the code from running
+    } else {
+        guessedLetters.push(userGuess);
+    }
+
+
+
+
+    // // each letter key pressed decreases the allowed number of guessed 
+    // guessesLeft--;
+    //     console.log(guessesLeft);
+    // document.getElementById("guesses-remaining").innerHTML = ("Guesses Remaining: " + guessesLeft);
 
     // check each userGuess against each index (each letter) of splitWord 
-        // for loop to run through the splitWord.length 
-            // if userGuess matches a value in splitWord array
-                // replace (_) in currentWord at corresponding index(es)
-            // else, (userGuess does not match a value in splitWord arrar)
-                // do nothing 
 
-    // (if player correctly guesses all the letters within # of guessesLeft ==> USER WINS)
-      // IF, all userGuesses = splitWord.length (all userGuesses were matched to splitWord) AND < guessesLeft 
-        // user wins
-            // increment the number of wins
-        
-    // (if player does not correctly guess all the letters within # of guessesLeft ==> USER LOSES)        
-      // ELSE, all userGuesses /= splitWord.length AND > guessesLeft 
-        // user loses
-            // decrement the number of wins 
-            // increment the number of losses
+    var matchFound = false;
+
+        // for loop to run through the splitWord.length 
+            for (j=0; j < splitWord.length; j++) {
+                // if userGuess matches a value in splitWord array
+                if (splitWord[j] === userGuess) {
+                    //remove letter from split word
+                    splitWord[j] = "_";
+                    //add letter to 
+                    blanksArray[j] = userGuess;
+                    matchFound = true; 
+                    correctLetters++;
+                }
+            }
+            document.getElementById("guessing-word").innerHTML = blanksArray.join(" ");
+            console.log(blanksArray);
+
+            // check if a match was found or not
+                // if found, check if all the matches have been found
+                if (matchFound) {
+                    if (totalLetters === correctLetters) {
+                        wins++;
+                        document.getElementById("wins").innerHTML = wins;
+                        //reset the game
+                    } 
+                } else {
+                    if (!guessCount) {
+                        losses++;
+                        // update DOM
+                        // reset the game 
+                    } else {
+                        guessCount--;
+                        // update DOM
+                    }
+                }
+                // yes - WIN
+                    //call func to restart
+                // else, check if guesses are available
+                // no - LOSS  
+                    //call func to restart 
+
+//func to restart the game
+            //reset variables
+            //update DOM
+
 }
 
-QUESTIONS: 
+// QUESTIONS: 
 // 1. set max number of guessesLeft?
 // 2. user cannot key same letter choice
 
