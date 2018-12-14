9a8ffbb1b403a958b1e3d77eb560c5d94b3ed2a2
LAMorris <leahmorris@Leahs-MacBook-Pro.local>
2018-12-14 01:47:18 -0600

    finally found a way for game to stop and count a loss when user runs out of guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -79,15 +79,19 @@ document.onkeyup = function(event) { // when user releases a key, the event func
 
     for (j=0; j < splitWord.length; j++) {  // for loop to run through the splitWord.length
         if (splitWord[j] === userGuess) {  // states if userGuess matches a value in splitWord array, then:
-            // splitWord[j] = "_"; 
             blanksArray[j] = userGuess; // add the matching userGuess letter to the blanksArray at specific/matching index position(s)
             matchFound = true; // updating the value of matchFound
             correctLetters++; // increment the value of correctLetters
-            console.log("number of correct letters: " + correctLetters);
+            console.log("correct letters: " + correctLetters);
         }
     }
         document.getElementById("guessing-word").innerHTML = blanksArray.join(" "); // replacing matched letters in HTML at specific index - so visible to user
-            console.log(blanksArray); // visualize in console the blanksArray with matched letter choice 
+            console.log("this is the blanksArray: " + blanksArray); // visualize the blankArray in console
+        
+    guessCount--; //decrement the guesses remaining with each userGuess
+    console.log("number of guesses left: " + guessCount); // visualize number in console 
+        document.getElementById("guesses-remaining").innerHTML = ("Guesses Remaining: " + guessCount); // update count in HTML
+
 
     if (matchFound) {  // check if a match was found or not
         if (totalLetters === correctLetters) { // if match is found AND all of the matches have been found
@@ -95,23 +99,25 @@ document.onkeyup = function(event) { // when user releases a key, the event func
             document.getElementById("wins").innerHTML = ("Wins: " + wins); //update win count in HTML
             //reset the game
         } 
-    } else if (!guessCount){  // if no match was found
-          // if no match was found AND guessCount = 0
+    } else if (!guessCount) {  // if no match was found
+             // if no match was found AND guessCount = 0
             losses++; // increment number of losses
-            document.getElementById("losses").innerHTML = ("Losses: " + wins); // update win count in HTML
+            document.getElementById("losses").innerHTML = ("Losses: " + losses); // update win count in HTML
             // reset the game 
-        } else { // if no match was found, but user stills has guesses
-                guessCount--; // decrement number of guesses 
-                console.log("guessCount: " + guessCount); // visualize what the current guessCount is
-                document.getElementById("guesses-remaining").textContent = ("Guesses Remaining: " + guessCount); 
+        } else { 
                 //reset the game
-            }   
-          }
+            }               
+    
+    if (guessCount === 0) { // added so that game ends when user runs out of guesses
+        losses++; // increment losses by one
+        document.getElementById("losses").innerHTML = ("Losses: " + losses); // display in HTML
+    } else {
+        //reset the game
+    }
+}
 
 
 
 //func to restart the game
             //reset variables
             //update DOM
-
-
